a8e355df21abca0a3b43c3958f1b94d8a22470ae
Kashan Ahmad <kashanahmad155@gmail.com>
2025-01-01 23:20:39 +0500

    Order table Migration fixed

--- database/migrations/2024_12_29_132208_create_orders_table.php
+++ database/migrations/2024_12_29_132208_create_orders_table.php
@@ -12,14 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();  // Auto-incrementing ID
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');  // Foreign key to the users table
-            $table->string('address');  // Billing or shipping address
-            $table->string('payment_method');  // Payment method (e.g., PayPal, Credit Card)
-            $table->decimal('total_amount', 10, 2);  // Total order amount
-            $table->enum('status', ['pending', 'processing', 'completed', 'cancelled'])->default('pending');  // Order status
+            $table->id(); 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
+            $table->string('address'); 
+            $table->string('payment_method');  
+            $table->decimal('total_amount', 10, 2);
+            $table->enum('status', ['pending', 'processing', 'completed', 'cancelled'])->default('pending'); 
             $table->json('products');  // JSON column to store products and quantities
-            $table->timestamps();  // Created and updated timestamps
+            $table->timestamps();  
         });
     }
 
